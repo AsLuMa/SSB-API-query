fe023242eecbbf1cde9baef5974638ec4448e967
asluma <ase.mattson@gmail.com>
2021-12-17 14:45:16 +0100

    - testing various query methods on DataFrames

--- read_from_file.py
+++ read_from_file.py
@@ -8,8 +8,34 @@ import time
 import json
 import matplotlib.pyplot as plt
 
+# ---- # Hente inn dataset frå fil
 df_fastlege = pd.read_csv('temp_storage.txt')
-print(df_fastlege.to_string())
+# print(df_fastlege.to_string())
+
+
+# ---- # Nokre print-statements for å sjå på data
+# print(df_fastlege.kjønn)
+# print(df_fastlege[90:110].to_string())
+
+# ---- # Hente ut spesifikk rad, etter verdi frå kolonne
+# print(df_fastlege.loc[df_fastlege['kjønn'] == 'Kvinner'].to_string())
+# print(df_fastlege.loc[df_fastlege['alder'] == '30-49 år'].to_string())
+
+# ---- # setter to kolonner som index
+kjønn_og_alder_som_index = df_fastlege.set_index(['kjønn', 'alder']).sort_index(inplace=False)
+# print(kjønn_og_alder_som_index.to_string())
+
+# ---- # Henter bestemt kjønn og alder frå kolonne, basert på index satt over
+print(kjønn_og_alder_som_index.loc['Menn', '16-19 år'].to_string())
+
+# ---- # Ymse selects, projects og joins
 
 
 
+
+
+# print(df_fastlege.isin(['Menn', 'kjønn']).to_string())
+
+
+
+# matplotlib-stuff
